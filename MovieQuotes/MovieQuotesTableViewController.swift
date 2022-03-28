e8f69e814640e1cb763a882c56a68d495433a6e8
rhit-wangy51 <wangy51@rose-hulman.edu>
2022-03-28 09:19:08 -0400

    Functional list in the table view

--- MovieQuotes/MovieQuotesTableViewController.swift
+++ MovieQuotes/MovieQuotesTableViewController.swift
@@ -7,10 +7,17 @@
 
 import UIKit
 
+class MovieQuoteTableViewCell: UITableViewCell{
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    @IBOutlet weak var movieLabel: UILabel!
+}
+
 class MovieQuotesTableViewController: UITableViewController {
     
     let kMovieQuoteCell = "MovieQuoteCell"// k for constant
-    let names = ["Helen", "Lisa", "Peter","Dave"]
+//    let names = ["Helen", "Lisa", "Peter","Dave"]
+    var movieQuotes = [MovieQuote]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +27,12 @@ class MovieQuotesTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        //Hard code some movie quotes
+        let mq1 = MovieQuote(quote: "I'll be back", movie: "The Terminator")
+        let mq2 = MovieQuote(quote: "Yo Adrian", movie: "Rocky")
+        movieQuotes.append(mq1)
+        movieQuotes.append(mq2)
     }
     
     
@@ -28,16 +41,21 @@ class MovieQuotesTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // return the number of rows
-        return names.count
+        return movieQuotes.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: self.kMovieQuoteCell, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.kMovieQuoteCell, for: indexPath) as! MovieQuoteTableViewCell
 
 //         Configure the cell...
 //        cell.textLabel?.text = "This is row \(indexPath.row)"//indexPath has only .row and .session
-        cell.textLabel?.text = names[indexPath.row]
+//        cell.textLabel?.text =[indexPath.row]
+//        cell.textLabel?.text = movieQuotes[indexPath.row].quote
+//        cell.detailTextLabel?.text = movieQuotes[indexPath.row].movie
+        
+        cell.quoteLabel.text = movieQuotes[indexPath.row].quote
+        cell.movieLabel.text = movieQuotes[indexPath.row].movie
 
         return cell
     }
